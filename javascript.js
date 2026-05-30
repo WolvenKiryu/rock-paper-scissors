fa5089041c491ae98fb6e84afef18454ff5c4538
WolvenKiryu <wolfcat22@gmail.com>
2026-05-30 14:18:12 -0500

    Add getComputerChoice to randomly chose computer's play

--- javascript.js
+++ javascript.js
@@ -1,1 +1,24 @@
-console.log("Hello World!");
+console.log("Let's begin!");
+
+function getComputerChoice() {
+    let computerChoice;
+    let ranChoice = Math.random() * 3;
+    if (ranChoice <= 1) {
+        computerChoice = "rock";
+    } else if (ranChoice > 1 && ranChoice <= 2) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+    console.log("The computer chose... " + computerChoice + "!");
+}
+
+getComputerChoice();
+
+
+
+// Create a function called getComputerChoice to calculate the PC's play
+// getComputerChoice will return a random string value of "rock", "paper" or "scissors"
+// Create a function called getHumanChoice to receive the player's play
+// Use a prompt to receive the player's play via getHumanChoice
+// 
